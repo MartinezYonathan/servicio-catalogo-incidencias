e0c310c0341507557edcbb118bc429dc64cc69bd
fernanda-rosales <chica_priston066@hotmail.com>
2021-08-26 22:38:12 -0500

    init service catalogy

---
+++ src/main/java/edu/mx/uacm/incidencias/ServicioCatalogoIncidenciasApplication.java
@@ -0,0 +1,13 @@
+package edu.mx.uacm.incidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCatalogoIncidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioCatalogoIncidenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mx/uacm/incidencias/ServicioCatalogoIncidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mx.uacm.incidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioCatalogoIncidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
